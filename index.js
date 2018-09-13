fdde1ff517454575e27b4ed36a96044438d5eb03
cjaramgo <43120125+cjaramgo@users.noreply.github.com>
2018-09-13 06:36:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Prueba gesti&#243;n de configuración");
+    response.end("Hola Mundo, prueba CM-Eafit");
 
 });
 
